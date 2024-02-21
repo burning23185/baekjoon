28cb0b9d3c0d68306196f56cb8c3d11cd2769e2d
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-02-21 16:25:25 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -7,23 +7,19 @@ class Solution:
 
     def doMale(self, num, switches):
         for i in range(num-1, len(switches), num):
-            if switches[i]:
-                switches[i] = 0
-                continue
-            switches[i] = 1
+            switches[i] = 1 - switches[i]
 
     def doFemale(self, num, switches):
 
         start, end = num - 2, num
-        switch_value = lambda x : 0 if switches[x] else 1
-        switches[num-1] = switch_value(num-1)
+        switches[num-1] = 1- switches[num-1]
 
         while start >= 0 and end < len(switches):
             if switches[start] != switches[end]:
                 break
 
-            switches[start] = switch_value(start)
-            switches[end] = switch_value(end)
+            switches[start] = 1 - switches[start]
+            switches[end] = 1 - switches[end]
             start -= 1
             end += 1
 
