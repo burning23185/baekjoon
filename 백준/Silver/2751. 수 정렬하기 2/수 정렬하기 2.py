f8b5fc5cc90379bfabf39389964b1bccd2f9fba3
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-02-22 12:07:58 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 3880 ms, Memory: 159080 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -5,57 +5,38 @@ class Solution:
         self.input = sys.stdin.readline
         self.output = sys.stdout.write
     
-    def merge_sort(self, arr):
+    def _merge(self, arr1, arr2):
+        result = []
+        i = j = 0
 
-        #탈출 조건
-        if len(arr) < 2:
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] < arr2[j]:
+                result.append(arr1[i])
+                i += 1
+            else:
+                result.append(arr2[j])
+                j += 1
+
+        while i < len(arr1):
+            result.append(arr1[i])
+            i += 1
+
+        while j < len(arr2):
+            result.append(arr2[j])
+            j += 1
+
+        return result
+    
+    def mergeSort(self, arr):
+
+        if len(arr) <= 1:
             return arr
 
-        pivot = len(arr) // 2
-        left_arr = self.merge_sort(arr[:pivot])
-        right_arr = self.merge_sort(arr[pivot:])
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
 
-        merged_arr = []
-        l_idx, r_idx = 0, 0
-
-        """
-        작은 수부터 병합된 리스트에 집어넣기
-            1 4 5 // 1, 3, 8
-            merged = []
-            
-            1 4 5 // x, 3, 8
-            merged = [1]
-
-            x 4 5 // x, 3, 8
-            merged = [1,1]
-
-            x 4 5 // x, x, 8
-            merged = [1,1,3]
-
-            x x 5 // x, x, 8
-            merged = [1,1,3,4]
-
-            x x x // x, x, 8
-            merged = [1,1,3,4,5]
-
-            x x x // x, x, x
-            merged = [1,1,3,4,5,8]
-        종료
-        """ 
-
-        while l_idx < len(left_arr) and r_idx < len(right_arr):
-            if left_arr[l_idx] < right_arr[r_idx]:
-                merged_arr.append(left_arr[l_idx])
-                l_idx += 1
-            else:
-                merged_arr.append(right_arr[r_idx])
-                r_idx += 1
-
-        #왼쪽, 오른쪽 병합
-        merged_arr += left_arr[l_idx:]
-        merged_arr += right_arr[r_idx:]
-
-        return merged_arr   
+        return self._merge(self.mergeSort(L), self.mergeSort(R))
 
     def answer(self):
         N = int(self.input())
@@ -64,7 +45,7 @@ class Solution:
         for _ in range(N):
             res.append(int(self.input()))
             
-        self.output('\n'.join(str(i) for i in self.merge_sort(res)) + '\n')
+        self.output('\n'.join(str(i) for i in self.mergeSort(res)) + '\n')
 
 s = Solution()
 s.answer()
