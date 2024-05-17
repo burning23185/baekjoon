3429190a12cc751fc294ff655dd4cc825b8d83b5
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-05-17 23:01:13 +0900

    [D3] Title: 햄버거 다이어트, Time: 629 ms, Memory: 56,104 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -0,0 +1,40 @@
+def dfs(idx, temp_res = 0, temp_kal = 0):
+    global res
+
+    if temp_kal > L:
+        return
+    
+    res = temp_res if res < temp_res else res
+
+    #마지막 인덱스까지 내려왔다면 리턴
+    if idx == N :
+        return
+    
+    score, calorie = ingredients[idx]
+
+def dfs(idx, temp_res = 0, temp_kal = 0):
+    global res
+
+    if temp_kal > L:
+        return
+    
+    res = temp_res if res < temp_res else res
+
+    #마지막 인덱스까지 내려왔다면 리턴
+    if idx == N :
+        return
+    
+    score, calorie = ingredients[idx]
+
+    dfs(idx+1, temp_res + score, temp_kal + calorie)
+    dfs(idx+1, temp_res, temp_kal)
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N, L = map(int, input().split())
+    ingredients = [list(map(int, input().split())) for _ in range(N)]
+    res = 0
+    dfs(0)
+
+    print("#{0} {1}".format(test_case, res))
