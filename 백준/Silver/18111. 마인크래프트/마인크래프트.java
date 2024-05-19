2f2b3fedf16f29ef02abcb5f3864686de2469f2e
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-05-19 14:32:53 +0900

    [Silver II] Title: 마인크래프트, Time: 540 ms, Memory: 39596 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -6,13 +6,16 @@ public class Main {
     private static int b;
     private static int[][] nums;
 
-    static void input() throws IOException{
+    static int[] input() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         String[] inputs = br.readLine().split(" ");
         n = Integer.parseInt(inputs[0]);
         m = Integer.parseInt(inputs[1]);
         b = Integer.parseInt(inputs[2]);
+
+        int min = 255;
+        int max = 0;
 
         nums = new int[n][m];
 
@@ -21,9 +24,12 @@ public class Main {
 
             for(int j = 0 ; j < inputs.length ; j++){
                 nums[i][j] = Integer.parseInt(inputs[j]);
+                min = nums[i][j] < min ? nums[i][j] : min;
+                max = nums[i][j] > max ? nums[i][j] : max;
             }
         }
         br.close();
+        return new int[]{min, max};
     }
 
     //target 높이까지 평탄화에 걸리는 시간 계산 (해당 높이를 만들 수 없는 경우 return -1)
@@ -44,25 +50,29 @@ public class Main {
     }
 
     //0 ~ 256 높이 까지 평탄화 시도 후 최소시간 및 높이 반환
-    static int[] calFunc(){
+    static int[] calFunc(int min, int max){
         int min_time = Integer.MAX_VALUE;
         int res = 0;
         int temp_res = 0;
 
-        for(int i = 0 ; i <= 256 ; i++){
+        for(int i = min ; i <= max ; i++){
 
             temp_res = smoothFunc(i);
-            if(temp_res < 0 ||temp_res > min_time) continue;
+            if(temp_res < 0) break; 
+
+            if(temp_res > min_time) continue;
 
             res = i;
             min_time = temp_res;
         }
+
         return new int[]{min_time, res}; 
     }
 
+
     public static void main(String[] args) throws IOException{
-        input();
-        int[] result = calFunc();
+        int[] min_max = input();
+        int[] result = calFunc(min_max[0], min_max[1]);
         System.out.println(String.format("%d %d", result[0], result[1]));
     }
 }
