ca44427b573d9e92bab90d3e617906f3e982cc6b
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-05-28 19:38:03 +0900

    [level 2] Title: 요격 시스템, Time: 0.43 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -5,13 +5,14 @@ class Solution {
         if(targets.length < 1) return 0;
 
         Arrays.sort(targets, (a, b) -> a[1] - b[1]);
-        int answer = 1;
-        int end_idx = targets[0][1] - 1;
+        int answer = 0;
+        int before = 0;
 
-        for (int i = 1 ; i < targets.length ; i++) {
-            if (targets[i][0] <= end_idx && end_idx <= targets[i][1]) continue;
-            answer++;
-            end_idx = targets[i][1] - 1;
+        for(int i=0; i<targets.length; i++){
+            if(before <= targets[i][0]){
+                before = targets[i][1];
+                answer++;
+            }
         }
         return answer;
     }
