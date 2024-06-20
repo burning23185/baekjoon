68d6a2a144186473a1b99490e2a38480d18f6c73
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-06-20 18:37:35 +0900

    [Silver I] Title: 카드 구매하기, Time: 120 ms, Memory: 14464 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n+1];
+        String[] inputs = br.readLine().split(" ");
+        for(int i = 1 ; i <= n ; i++) nums[i] = Integer.parseInt(inputs[i-1]);
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + nums[j]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
