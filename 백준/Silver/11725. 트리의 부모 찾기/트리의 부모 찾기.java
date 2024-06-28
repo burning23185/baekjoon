183a1df20bb05e6617f91c2ec851053ca5806511
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-06-28 17:07:36 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 2376 ms, Memory: 213840 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,13 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    private static ArrayList<Integer>[] adj;
+    private static Map<Integer, List<Integer>> adj;
     private static int[] parents;
 
     static void dfs(int now){
-        for(int n : adj[now]){
+        for(int n : adj.get(now)){
             if(parents[now] == n) continue;
             parents[n] = now;
             dfs(n);
@@ -20,9 +23,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         String[] inputs;
-        adj = new ArrayList[n+1];
-        for (int i = 1; i <= n; i++) adj[i] = new ArrayList<>();
-
+        adj = new HashMap<>();
         parents = new int[n+1];
         int a, b;
 
@@ -30,9 +31,24 @@ public class Main {
             inputs = br.readLine().split(" ");
             a = Integer.parseInt(inputs[0]);
             b = Integer.parseInt(inputs[1]);
-            adj[a].add(b);
-            adj[b].add(a);
+
+            if(adj.containsKey(a)){
+                adj.get(a).add(b);
+            }else{
+                List<Integer> temp = new ArrayList<>();
+                temp.add(b);
+                adj.put(a, temp);
+            }
+
+            if(adj.containsKey(b)){
+                adj.get(b).add(a);
+            }else{
+                List<Integer> temp = new ArrayList<>();
+                temp.add(a);
+                adj.put(b, temp);
+            }
         }
+
         dfs(1);
         for(int i = 2; i <= n ; i++) sb.append(String.format("%d%n", parents[i]));
         System.out.println(sb);
