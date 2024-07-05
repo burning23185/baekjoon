31d794f4a90236f604295d82328d8031e62055c1
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-07-05 22:26:09 +0900

    [level 3] Title: 입국심사, Time: 0.05 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 class Solution {
     private boolean checkUnder(long mid, int n, int[] times){
         long complete = 0;
@@ -11,20 +10,19 @@ class Solution {
         
     public long solution(int n, int[] times) {
         long answer = 0;
-        long low = 0;
-        // N 명의 사람을 모두 통과시킬 수 있는 가장 긴 시간
-        long high = Long.MAX_VALUE;
+        long left = 0;
+        long right = Long.MAX_VALUE;
         long mid;
         
-        while(low <= high) {
-            mid = (low + high)/2;
+        while(left <= right) {
+            mid = (left + right)/2;
             
             if (checkUnder(mid, n, times)){
-                low = mid + 1;
+                left = mid + 1;
                 continue;
             } // 해당 시간에는 모든 사람이 검사받을 수 없다.
 
-            high = mid - 1;
+            right = mid - 1;
             answer = mid; // 모두 검사받았으나, 더 최솟값이 있을 수 있다.
         }
         return answer;
