06f99b196693d042b5cf3840af6ce2dfd55cd651
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-08-30 21:25:56 +0900

    [level 2] Title: 디펜스 게임, Time: 97.93 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,35 +1,21 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    private boolean checkFunc(int n, int k, int target, int[] enemy){
-        int res = n;
-        List<Integer> enemies = new ArrayList<>();
-        for(int i = 0 ; i < target ; i++) enemies.add(enemy[i]);
-        enemies.sort(Comparator.reverseOrder());
-
-        for(int i = k ; i < enemies.size() ; i++){
-            res -= enemies.get(i);
-            if(res < 0) return false;
-        }
-        return true;
-    }
     public int solution(int n, int k, int[] enemy) {
         if (enemy.length <= k) return enemy.length;
-        int left = 0 , right = enemy.length;
-        int mid;
         int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
 
-        while(left <= right){
-            mid = (left+right)/2;
-            if(checkFunc(n, k, mid, enemy)){
-                left = mid + 1;
-                answer = mid;
-                continue;
-            }
-            right = mid - 1;
+        for (int e : enemy) {
+            queue.add(e);
+            answer++;
+            n -= e;
+            if(n >= 0) continue;
+            if(k == 0) return answer-1;
+
+            n = n + queue.poll();
+            k--;
         }
-        return answer;
+        return enemy.length;
     }
 }
