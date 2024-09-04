917f27fdfb679e530a046b11d123bb123e35fa0c
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-09-04 18:07:09 +0900

    [level 1] Title: 신고 결과 받기, Time: 4674.64 ms, Memory: 516 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,60 +1,21 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
-    static class User{
-        private String name;
-        private int reported;
-        private boolean ban;
-
-        public User(String name){
-            this.name = name;
-            this.reported = 0;
-            this.ban = false;
-        }
-
-        public void increaseReported(){
-            this.reported++;
-        }
-
-        public void updateBan(boolean ban){
-            this.ban = ban;
-        }
-    }
     public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, List<User>> users = new HashMap<>();
-        Map<String, User> reported = new HashMap<>();
-
-        for(String id : id_list) {
-            users.put(id, new ArrayList<>());
-            reported.put(id, new User(id));
+        List<String> list = Arrays.stream(report).distinct().collect(Collectors.toList());
+        HashMap<String, Integer> count = new HashMap<>();
+        for (String s : list) {
+            String target = s.split(" ")[1];
+            count.put(target, count.getOrDefault(target, 0) + 1);
         }
 
-        //신고 처리
-        Set<String> reports = new HashSet<>();
-        String[] temp;
-        User reported_user;
-        for(String r : report){
-            if(!reports.add(r)) continue;
-
-            temp = r.split(" ");
-            reported_user = reported.get(temp[1]);
-            reported_user.increaseReported();
-            users.get(temp[0]).add(reported_user);
-        }
-
-        //신고 횟수 기준 정지 처리
-        for(String id : id_list){
-            if(reported.get(id).reported < k) continue;
-            reported.get(id).updateBan(true);
-        }
-
-        //신고한 회원 기준 정지 메일수 산정
-        int[] answer = new int[id_list.length];
-        for(int i = 0 ; i < id_list.length ; i++){
-            for(User u : users.get(id_list[i])){
-                if(u.ban) answer[i]++;
-            }
-        }
-        return answer;
+        return Arrays.stream(id_list).map(_user -> {
+            final String user = _user;
+            List<String> reportList = list.stream().filter(s -> s.startsWith(user + " ")).collect(Collectors.toList());
+            return reportList.stream().filter(s -> count.getOrDefault(s.split(" ")[1], 0) >= k).count();
+        }).mapToInt(Long::intValue).toArray();
     }
 }
