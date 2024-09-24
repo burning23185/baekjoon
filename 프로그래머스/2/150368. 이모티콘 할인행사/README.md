77884647c61a6e463a66c5f7c3b2cc7974a6ff7b
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-09-24 19:33:40 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 53.19 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/README.md
+++ 프로그래머스/2/150368. 이모티콘 할인행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이모티콘 할인행사 - 150368 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150368?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150368) 
 
 ### 성능 요약
 
-메모리: 87 MB, 시간: 74.52 ms
+메모리: 90.5 MB, 시간: 53.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 24일 16:54:49
+2024년 09월 24일 19:33:36
 
 ### 문제 설명
 
--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,31 +1,26 @@
 class Solution {
     int[] discounts;
     int[] res;
+    private void checkCondition(int[][] users, int[] emoticons){
+        int[] temp_res = new int[]{0, 0};
 
-    private void countAvailable(int[][] users, int[] emoticons, int depth) {
+        for (int[] user : users) {
+            double totalBuy = 0;
 
-        if (depth == emoticons.length) {
-            int[] temp_res = new int[]{0, 0}; // 플러스 가입자 수와 총 이익을 저장할 배열
-
-            for (int[] user : users) {
-                double totalBuy = 0;
-
-                for (int i = 0; i < emoticons.length; i++) {
-                    if (user[0] <= discounts[i]) { // 사용자가 원하는 최소 할인율을 만족하는 경우
-                        totalBuy += emoticons[i] * (100 - discounts[i]) / 100.0;
-                    }
-                }
-
-                if (totalBuy >= user[1]) {
-                    temp_res[0]++;
-                } else {
-                    temp_res[1] += (int) totalBuy;
-                }
+            for (int i = 0; i < emoticons.length; i++) {
+                if (user[0] <= discounts[i]) // 사용자의 구매 조건을 만족하는 경우
+                    totalBuy += emoticons[i] * (100 - discounts[i]) / 100.0;
             }
 
-            // 플러스 가입자 수가 더 많거나, 동일한 경우 더 많은 이익이면 갱신
-            if (temp_res[0] > res[0] || (temp_res[0] == res[0] && temp_res[1] > res[1])) res = temp_res;
-
+            if (totalBuy >= user[1]) temp_res[0]++;
+            else temp_res[1] += (int) totalBuy;
+        }
+        // 플러스 가입자 수가 더 많거나, 동일한 경우 더 많은 이익이면 갱신
+        if (temp_res[0] > res[0] || (temp_res[0] == res[0] && temp_res[1] > res[1])) res = temp_res;
+    }
+    private void countAvailable(int[][] users, int[] emoticons, int depth) {
+        if (depth == emoticons.length) {
+            checkCondition(users, emoticons);
             return;
         }
 
