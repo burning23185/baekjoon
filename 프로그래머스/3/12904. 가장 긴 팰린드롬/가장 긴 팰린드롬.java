3eb250be6f04ba1d205ef7600e35e3b8f40d016c
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-10-29 18:15:45 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 15.57 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,39 +1,20 @@
-
-import java.util.*;
-class Solution
-{
-    public int solution(String s)
-    {
-       int left = 0, right = 0;
-        int result = 1;
-
-        if(s.length() != 1) {
-            for (int center = 1; center < s.length() - 1; center++) {
-                left = center - 1;
-                right = center + 1;
-                while (left >= 0 && right <= s.length() - 1) {
-                    if (s.charAt(left) != s.charAt(right))
-                        break;
-
-                    result = right - left + 1 > result ? right - left + 1 : result;
-                    left--;
-                    right++;
-                }
-            }
-
-            for (int center = 0; center <= s.length() - 2; center++) {
-                left = center;
-                right = center + 1;
-                while (left >= 0 && right <= s.length() - 1) {
-                    if (s.charAt(left) != s.charAt(right))
-                        break;
-                    result = right - left + 1 > result ? right - left + 1 : result;
-                    left--;
-                    right++;
-                }
-            }
+class Solution {
+    public int solution(String s) {
+        int maxLength = 1;
+        
+        for (int center = 0; center < s.length(); center++) {
+            maxLength = Math.max(maxLength, expandAroundCenter(s, center, center));
+            maxLength = Math.max(maxLength, expandAroundCenter(s, center, center + 1));
         }
 
-        return result;
+        return maxLength;
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 }
