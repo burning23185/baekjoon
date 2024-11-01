08037aa2abdd92fbf7193ee40073a178ace08a19
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-11-01 15:56:56 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 18.29 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,8 +1,6 @@
 import java.util.*;
 
-class Solution {    
-    private StringBuilder sb = new StringBuilder();
-    
+class Solution {        
     private int parseTimeToSec(String time){
         String[] s = time.split(":");
         return Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);  
@@ -11,25 +9,25 @@ class Solution {
     private String parseSecToTime(int sec){
         int mm = sec/60;
         int ss = sec%60;
-        sb.setLength(0);
-        if(mm < 10) sb.append('0');
-        sb.append(mm).append(':');
-        if(ss < 10) sb.append('0');
-        sb.append(ss);
-        return sb.toString();
+        String res = "";
+        
+        if(mm < 10) res += "0";
+        res += mm + ":";
+        if(ss < 10) res += "0";
+        res += ss;
+        return res;
     }
     
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {     
-        int op_start_time = parseTimeToSec(op_start);
-        int op_end_time = parseTimeToSec(op_end);
+        int[] op_time = new int[]{parseTimeToSec(op_start), parseTimeToSec(op_end)};
         int start_pos = parseTimeToSec(pos);
-        int now = ((op_start_time <= start_pos) && (start_pos <= op_end_time)) ? op_end_time : start_pos; 
+        int now = ((op_time[0] <= start_pos) && (start_pos <= op_time[1])) ? op_time[1] : start_pos; 
         int end_video = parseTimeToSec(video_len);
         
         for(String cmd : commands){
-            if(cmd.equals("prev")) now = now < 10 ? 0 : now-10;
-            else if(cmd.equals("next")) now = now + 10 > end_video ? end_video : now + 10;
-            now = ((op_start_time <= now) && (now <= op_end_time)) ? op_end_time : now; 
+            if(cmd.equals("prev")) now = Math.max(now - 10, 0);
+            else if(cmd.equals("next")) now = Math.min(now + 10, end_video); 
+            now = ((op_time[0] <= now) && (now <= op_time[1])) ? op_time[1] : now; 
         }
         
         return parseSecToTime(now);
